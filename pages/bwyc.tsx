56b94972170498cd8d9294495c60435b78104686
BWYC <129849126+BWYC@users.noreply.github.com>
2023-12-13 16:48:32 +0000

    1

--- pages/bwyc.tsx
+++ pages/bwyc.tsx
@@ -1,7 +1,8 @@
-import { useContract, useNFTs, useAddress, useOwnedNFTs, ThirdwebNftMedia } from "@thirdweb-dev/react";
+import { useContract, useNFTs, useAddress, useOwnedNFTs, ThirdwebNftMedia,  useValidDirectListings,
+  useValidEnglishAuctions } from "@thirdweb-dev/react";
 import Container from "../components/Container/Container";
 import NFTGrid from "../components/NFT/NFTGrid";
-import { NFT_COLLECTION_ADDRESS2 } from "../const/contractAddresses";
+import { NFT_COLLECTION_ADDRESS2, MARKETPLACE_ADDRESS } from "../const/contractAddresses";
 import ProfilePage from "./profile/[address]";
 import styles from "../styles/Profile.module.css";
 import Link from "next/link";
@@ -13,24 +14,34 @@ import React, { useState } from "react";
 import TokenPage from "./token/[contractAddress]/[tokenId]";
 import { color } from "web3uikit";
 import {Twitter, Discord, UserTeam, ArrowDown, Dapps, Checkmark } from '@web3uikit/icons'
-import Data from "./collections/data1"
-import Skeleton from "../components/Skeleton/Skeleton";
+import Data from "./collections/data"
+import ListingWrapper from "../components/ListingWrapper/ListingWrapper";
 
 
-export default function Buy1() {
+export default function Buy() {
   // Load all of the NFTs from the NFT Collection
   const verfied = true
-  const { contract } = useContract(NFT_COLLECTION_ADDRESS2);
-  const { data: data, isLoading: isLoading } = useNFTs(contract, {
-    start: 0,
-    count: 100,
-  });
+  const address = useAddress();
+  const { contract: nftCollection } = useContract(NFT_COLLECTION_ADDRESS2);
 
-   const address = useAddress();
-   const { data: owned } = useOwnedNFTs(contract, address);
- 
+  const { contract: marketplace } = useContract(
+    MARKETPLACE_ADDRESS,
+    "marketplace-v3",
+  );
+
+  const { data: ownedNfts, isLoading: isLoading } = useOwnedNFTs(
+    nftCollection,
+    address,
+  );
+  const { data: directListings, isLoading: loadingDirects } =
+    useValidDirectListings(marketplace);
+
+  const { data: auctionListings, isLoading: loadingAuctions } =
+    useValidEnglishAuctions(marketplace);
+
    const [selectedNft, setSelectedNft] = useState<NFTType>();
 
+ 
 
   return (
     <Container maxWidth="lg">
@@ -39,23 +50,22 @@ export default function Buy1() {
         <div  style={{color: "white", height: "80px", width: "100%", padding: "1%", backdropFilter: "blur(100px)", borderRadius: "8px", fontSize: "20px", background: "rbg(0, 0, 0, 0.7)", display: "flex", flexDirection: "row", gap: "10px" }}>
         <Data />
         <p className={styles.verify}  style={{color: "white", width: "100%",  padding: "2%", borderRadius: "8px", display: "flex"}}>
-        BWYC II
+        RAR31ONES
        {verfied ? ( <>
         <Checkmark fontSize={20} style={{background: "green", padding: "1px", border: "dashed 1px", borderRadius: "100%"}} />
        </>)
           : (<></>)}</p>
-          <Link href={`/collections/${"0xF6C2584850Fd1F50fc979F72888Fb67CFa39a2C2"}`} style={{textAlign: "center", padding: "2%", border: "solid 1px gray", background: "rgba(0, 0, 50, 0.3)", fontSize: "14px", borderRadius: "8px", width: "30%", height: "60px"}}><p>VIEW ITEMS</p></Link>
+          <Link href={`/collections/${"0x69d5dDE5aF1fa6Cf5b86EC9B907c9bA1879c717f"}`} style={{textAlign: "center", padding: "2%", border: "solid 1px gray", background: "rgba(0, 0, 50, 0.3)", fontSize: "14px", borderRadius: "8px", width: "40%", height: "60px"}}><p>VIEW ITEMS</p></Link>
         </div>
-        <NFTGrid
-              data={data}
-              isLoading={isLoading}
-              overrideOnclickBehavior={(nft) => {
-                setSelectedNft(nft);
-              }}
-              emptyText={
-                "Looks like you don't own any BWYC in your Wallet. Buy some From DEX"
-              }
-            />
+        {loadingDirects ? (
+          <p>LOADING...</p>
+        ) : directListings && directListings.length === 0 ? (
+          <p style={{padding: "5%"}}>Nothing for sale yet! Head to the sell tab to list an NFT.</p>
+        ) : (
+          directListings?.map((listing) => (
+            <ListingWrapper listing={listing} key={listing.id} />
+          ))
+        )}
         </>
       ):(
 <>
