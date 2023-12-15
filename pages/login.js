bed09360248fbd81a26ca6d096e9998e6f0c7f0a
Thed3vel0p3r <151849465+Thed3vel0p3r@users.noreply.github.com>
2023-12-15 15:49:35 +0000

    121

--- pages/login.js
+++ pages/login.js
@@ -123,7 +123,7 @@ export async function getServerSideProps(context) {
     return {
       redirect: {
         destination: "/login",
-        permanent: true,
+        permanent: false,
       },
     };
   }
